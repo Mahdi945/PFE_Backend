13848f01d2fe682133f8bce4367920f10a32e7f0
Mahdi Bey <mahdibeyy@gmail.com>
2025-04-02 14:35:35 +0100

    v3 (01-04-2025)

--- station_service.sql
+++ station_service.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : dim. 23 mars 2025 à 18:38
+-- Généré le : mer. 02 avr. 2025 à 15:34
 -- Version du serveur : 10.4.28-MariaDB
 -- Version de PHP : 8.2.4
 
@@ -24,6 +24,35 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `affectations`
+--
+
+CREATE TABLE `affectations` (
+  `id` int(11) NOT NULL,
+  `pompiste_id` int(11) NOT NULL,
+  `poste_id` int(11) NOT NULL,
+  `pompe_id` int(11) NOT NULL,
+  `calendrier_id` int(11) NOT NULL,
+  `statut` enum('disponible','indisponible') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `calendrier`
+--
+
+CREATE TABLE `calendrier` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `statut` enum('disponible','occupé') NOT NULL,
+  `mois` int(11) NOT NULL,
+  `annee` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `contrats`
 --
 
@@ -33,8 +62,87 @@ CREATE TABLE `contrats` (
   `type_contrat` enum('individuel','organisationnel') NOT NULL,
   `solde_credit` decimal(10,2) NOT NULL,
   `date_debut` date DEFAULT NULL,
-  `duree_contrat` int(11) NOT NULL
+  `duree_contrat` int(11) NOT NULL,
+  `credit_utilise` decimal(10,2) DEFAULT 0.00
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `contrats`
+--
+
+INSERT INTO `contrats` (`id`, `id_utilisateur`, `type_contrat`, `solde_credit`, `date_debut`, `duree_contrat`, `credit_utilise`) VALUES
+(2, 15, 'individuel', 500.00, '2025-03-25', 12, 0.00);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `paiements`
+--
+
+CREATE TABLE `paiements` (
+  `id_paiement` int(11) NOT NULL,
+  `id_utilisateur` int(11) DEFAULT NULL,
+  `id_contrat` int(11) DEFAULT NULL,
+  `montant` decimal(10,2) DEFAULT NULL,
+  `date_paiement` datetime DEFAULT NULL,
+  `solde_restant` decimal(10,2) DEFAULT NULL,
+  `mode_paiement` enum('carte bancaire','virement','espèces','chèque') NOT NULL,
+  `statut_paiement` enum('réussi','échec','en attente') DEFAULT 'en attente'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pistolets`
+--
+
+CREATE TABLE `pistolets` (
+  `id` int(11) NOT NULL,
+  `pompe_id` int(11) NOT NULL,
+  `numero_pistolet` varchar(50) NOT NULL,
+  `index_ouverture` decimal(10,2) DEFAULT 0.00,
+  `index_fermeture` decimal(10,2) DEFAULT 0.00,
+  `statut` enum('disponible','indisponible') DEFAULT 'disponible',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pompes`
+--
+
+CREATE TABLE `pompes` (
+  `id` int(11) NOT NULL,
+  `numero_pompe` varchar(50) NOT NULL,
+  `type_pompe` enum('sans plomb','gasoil','autre') NOT NULL,
+  `statut` enum('disponible','indisponible') DEFAULT 'disponible',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `postes`
+--
+
+CREATE TABLE `postes` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `heure_debut` time NOT NULL,
+  `heure_fin` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `postes`
+--
+
+INSERT INTO `postes` (`id`, `nom`, `heure_debut`, `heure_fin`) VALUES
+(1, 'Matin', '06:00:00', '14:00:00'),
+(2, 'Après-midi', '14:00:00', '22:00:00'),
+(3, 'Nuit', '22:00:00', '06:00:00');
 
 -- --------------------------------------------------------
 
@@ -48,7 +156,8 @@ CREATE TABLE `transactions` (
   `id_utilisateur` int(11) DEFAULT NULL,
   `quantite` decimal(10,2) DEFAULT NULL,
   `montant` decimal(10,2) DEFAULT NULL,
-  `date_transaction` datetime DEFAULT NULL
+  `date_transaction` datetime DEFAULT NULL,
+  `id_contrat` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -63,21 +172,23 @@ CREATE TABLE `utilisateurs` (
   `email` varchar(255) NOT NULL,
   `numero_telephone` varchar(20) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `role` enum('gerant','Cogerant','caissier','pompiste','client') DEFAULT 'client',
+  `role` enum('gerant','Cogerant','caissier','pompiste','client') NOT NULL DEFAULT 'client',
   `temps_de_creation` timestamp NOT NULL DEFAULT current_timestamp(),
-  `status` enum('active','inactive') DEFAULT 'active'
+  `status` enum('active','inactive') DEFAULT 'active',
+  `photo` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `utilisateurs`
 --
 
-INSERT INTO `utilisateurs` (`id`, `username`, `email`, `numero_telephone`, `password`, `role`, `temps_de_creation`, `status`) VALUES
-(9, 'johndoe', 'johndoe@example.com', '1234567890', '$2b$10$ZTs0/GYkUz4R3c6.fqQbQuOaWbmaH0CJa9NCIlADeiDvF7e.urmFO', 'Cogerant', '2025-03-14 14:42:46', 'active'),
-(10, 'mahdov', 'jo@example.com', '1267898880', '$2b$10$r78JXVlJc7FO6ST/HS.o8ubQhjyFERrn/GfTlQzmvxcazkfl.pvWO', 'pompiste', '2025-03-14 15:26:28', 'active'),
-(13, 'Mahdi Bey', 'mahdibeyy20002@gmail.com', '56327280', '$2b$10$djxErd2eHbhS0PKTK4PuXeE/Y/JQvqmppg7kk2t0R6RjSSQLRbzO6', 'gerant', '2025-03-14 16:45:22', 'active'),
-(14, 'Ahmed Kozdoghli', 'mahdibey2002@gmail.com', '56327290', '$2b$10$L2HpA.14YbNSAULWPCBWs.10W6QOyk6TqCgfCEWwkWGVibaNU1g7W', 'client', '2025-03-17 13:43:35', 'active'),
-(15, 'Ahmed Bey', 'beyymahdi@gmail.com', '56327210', '$2b$10$XWfGw.9AIBWxvwwyvcKLfuEWcdrP.5xqXJjW7hEr9NNCWI5bdhF52', 'client', '2025-03-17 20:55:51', 'active');
+INSERT INTO `utilisateurs` (`id`, `username`, `email`, `numero_telephone`, `password`, `role`, `temps_de_creation`, `status`, `photo`) VALUES
+(9, 'john', 'johndoe@example.com', '1234567890', '$2b$10$ZTs0/GYkUz4R3c6.fqQbQuOaWbmaH0CJa9NCIlADeiDvF7e.urmFO', 'client', '2025-03-14 14:42:46', 'active', NULL),
+(10, 'mahdov', 'jo@example.com', '1267898880', '$2b$10$r78JXVlJc7FO6ST/HS.o8ubQhjyFERrn/GfTlQzmvxcazkfl.pvWO', 'pompiste', '2025-03-14 15:26:28', 'active', NULL),
+(13, 'Mahdi Bey', 'mahdibeyy20002@gmail.com', '56327280', '$2b$10$djxErd2eHbhS0PKTK4PuXeE/Y/JQvqmppg7kk2t0R6RjSSQLRbzO6', 'caissier', '2025-03-14 16:45:22', 'active', NULL),
+(14, 'Ahmed Ammar', 'mahdibeyy@gmail.com', '56327237', '$2b$10$3qvhoX.KKYtvZ5sIzXrgIORhFSgWR4XsR71VwaMGeiEcj.9her1/O', 'gerant', '2025-03-17 13:43:35', 'active', '/images/nbg.png'),
+(15, 'Ahmed Bey', 'mahdibey2002@gmail.com', '56327210', '$2b$10$0NjolxaLuID7fcKfLy8sOOYShGEjARg3L9BQN9spbEcnv8RkyrWZq', 'client', '2025-03-17 20:55:51', 'active', NULL),
+(16, 'mahdi', 'newuser@example.com', '12345678', '$2b$10$1d0Yfs65TEqFFE5acBSGb.QP7xQWQv6RcKzFGJFLVytMV8i5Pdhzm', 'client', '2025-03-29 10:18:29', 'active', NULL);
 
 -- --------------------------------------------------------
 
@@ -88,15 +199,42 @@ INSERT INTO `utilisateurs` (`id`, `username`, `email`, `numero_telephone`, `pass
 CREATE TABLE `vehicules` (
   `id` int(11) NOT NULL,
   `id_utilisateur` int(11) DEFAULT NULL,
-  `immatriculation` varchar(20) DEFAULT NULL,
+  `immatriculation` varchar(20) NOT NULL,
   `marque` varchar(50) DEFAULT NULL,
   `type_vehicule` varchar(50) DEFAULT NULL,
-  `qr_code` varchar(255) DEFAULT NULL
+  `qr_code` varchar(255) DEFAULT NULL,
+  `id_contrat` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `vehicules`
+--
+
+INSERT INTO `vehicules` (`id`, `id_utilisateur`, `immatriculation`, `marque`, `type_vehicule`, `qr_code`, `id_contrat`) VALUES
+(3, 15, '12234 TU 2999', 'Honda', 'Berline', 'http://localhost:3000/qrcodes/12234 TU 2999.png', 2),
+(4, 15, '122TU229', 'Mercedes', 'voiture', 'http://localhost:3000/qrcodes/122TU229.png', 2),
+(7, 15, '123TU229', 'Mercedes', 'voiture', 'http://localhost:3000/qrcodes/123TU229.png', 2);
 
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `affectations`
+--
+ALTER TABLE `affectations`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pompiste_id` (`pompiste_id`),
+  ADD KEY `poste_id` (`poste_id`),
+  ADD KEY `pompe_id` (`pompe_id`),
+  ADD KEY `calendrier_id` (`calendrier_id`);
+
+--
+-- Index pour la table `calendrier`
+--
+ALTER TABLE `calendrier`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_jour` (`date`);
 
 --
 -- Index pour la table `contrats`
@@ -106,12 +244,40 @@ ALTER TABLE `contrats`
   ADD KEY `id_utilisateur` (`id_utilisateur`);
 
 --
+-- Index pour la table `paiements`
+--
+ALTER TABLE `paiements`
+  ADD PRIMARY KEY (`id_paiement`),
+  ADD KEY `id_utilisateur` (`id_utilisateur`),
+  ADD KEY `id_contrat` (`id_contrat`);
+
+--
+-- Index pour la table `pistolets`
+--
+ALTER TABLE `pistolets`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pompe_id` (`pompe_id`);
+
+--
+-- Index pour la table `pompes`
+--
+ALTER TABLE `pompes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `postes`
+--
+ALTER TABLE `postes`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Index pour la table `transactions`
 --
 ALTER TABLE `transactions`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_vehicule` (`id_vehicule`),
-  ADD KEY `id_utilisateur` (`id_utilisateur`);
+  ADD KEY `id_utilisateur` (`id_utilisateur`),
+  ADD KEY `fk_transactions_contrats` (`id_contrat`);
 
 --
 -- Index pour la table `utilisateurs`
@@ -126,17 +292,56 @@ ALTER TABLE `utilisateurs`
 --
 ALTER TABLE `vehicules`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_utilisateur` (`id_utilisateur`);
+  ADD UNIQUE KEY `immatriculation` (`immatriculation`),
+  ADD UNIQUE KEY `qr_code` (`qr_code`),
+  ADD KEY `id_utilisateur` (`id_utilisateur`),
+  ADD KEY `fk_vehicules_contrats` (`id_contrat`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
+-- AUTO_INCREMENT pour la table `affectations`
+--
+ALTER TABLE `affectations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `calendrier`
+--
+ALTER TABLE `calendrier`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT pour la table `contrats`
 --
 ALTER TABLE `contrats`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `paiements`
+--
+ALTER TABLE `paiements`
+  MODIFY `id_paiement` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `pistolets`
+--
+ALTER TABLE `pistolets`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `pompes`
+--
+ALTER TABLE `pompes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `postes`
+--
+ALTER TABLE `postes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT pour la table `transactions`
@@ -148,17 +353,26 @@ ALTER TABLE `transactions`
 -- AUTO_INCREMENT pour la table `utilisateurs`
 --
 ALTER TABLE `utilisateurs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT pour la table `vehicules`
 --
 ALTER TABLE `vehicules`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Contraintes pour les tables déchargées
 --
+
+--
+-- Contraintes pour la table `affectations`
+--
+ALTER TABLE `affectations`
+  ADD CONSTRAINT `affectations_ibfk_1` FOREIGN KEY (`pompiste_id`) REFERENCES `utilisateurs` (`id`),
+  ADD CONSTRAINT `affectations_ibfk_2` FOREIGN KEY (`poste_id`) REFERENCES `postes` (`id`),
+  ADD CONSTRAINT `affectations_ibfk_3` FOREIGN KEY (`pompe_id`) REFERENCES `pompes` (`id`),
+  ADD CONSTRAINT `affectations_ibfk_4` FOREIGN KEY (`calendrier_id`) REFERENCES `calendrier` (`id`);
 
 --
 -- Contraintes pour la table `contrats`
@@ -167,9 +381,23 @@ ALTER TABLE `contrats`
   ADD CONSTRAINT `contrats_ibfk_1` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateurs` (`id`);
 
 --
+-- Contraintes pour la table `paiements`
+--
+ALTER TABLE `paiements`
+  ADD CONSTRAINT `paiements_ibfk_1` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateurs` (`id`),
+  ADD CONSTRAINT `paiements_ibfk_2` FOREIGN KEY (`id_contrat`) REFERENCES `contrats` (`id`);
+
+--
+-- Contraintes pour la table `pistolets`
+--
+ALTER TABLE `pistolets`
+  ADD CONSTRAINT `pistolets_ibfk_1` FOREIGN KEY (`pompe_id`) REFERENCES `pompes` (`id`);
+
+--
 -- Contraintes pour la table `transactions`
 --
 ALTER TABLE `transactions`
+  ADD CONSTRAINT `fk_transactions_contrats` FOREIGN KEY (`id_contrat`) REFERENCES `contrats` (`id`),
   ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`id_vehicule`) REFERENCES `vehicules` (`id`),
   ADD CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateurs` (`id`);
 
@@ -177,6 +405,7 @@ ALTER TABLE `transactions`
 -- Contraintes pour la table `vehicules`
 --
 ALTER TABLE `vehicules`
+  ADD CONSTRAINT `fk_vehicules_contrats` FOREIGN KEY (`id_contrat`) REFERENCES `contrats` (`id`),
   ADD CONSTRAINT `vehicules_ibfk_1` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateurs` (`id`);
 COMMIT;
 
